c6ce0c01438ac618d82623d8766620665e273cb4
Max玮 <2455829429@qq.com>
2023-11-30 12:41:45 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "aes.h"
+#include "fstream"
+#include "zbase64.h"
+//hack:今晚完成到了解密的测试，存在这样一个疑问点，解密输入的密文数组和预想的是转置的，这里需要解决并修复解密函数中的bug
+int main()
+{
+	aes tool;
+    ZBase64 base64;     aaaaaaaaaaaaaaaaaacccaa
+    string plain_str = "aaaaaaaaaaaaaaaaaacccaa";
+    int plainLen = 0;
+    unsigned char cipher[((strlen(plain_str.c_str())%16==0) ?(strlen(plain_str.c_str())+16) \
+	: ((int)(strlen(plain_str.c_str())/16)*16)+16)];
+    unsigned char plaintext[((strlen(plain_str.c_str())%16==0) ?(strlen(plain_str.c_str())+16) \
+    : ((int)(strlen(plain_str.c_str())/16)*16)+16)];
+
+    tool.initialization_vector("aaaaaaaaaaaaaaaa");
+    //*密钥扩展
+	tool.KeyExpansion("aaaaaaaaaaaaaaaaaaaaaaaa",tool.exp_key);
+    //*加密
+	string base64_cipher = tool.encrypt(plain_str,tool.exp_key,cipher,sizeof(cipher));
+
+    string strPlain = tool.decrypt(base64_cipher,tool.exp_key,plaintext,plainLen);
+
+    cout << "cipher is " << base64.Encode(cipher,sizeof(cipher))<< endl;
+    for(int i = 0; i < plainLen; i++)
+    {
+        cout << hex << (int)plaintext[i] << " ";
+    }
+    cout << endl << "plain is " << strPlain << endl;
+    cout << endl;
+
+    return 0;
+}
